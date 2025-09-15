7d4141a03219dd8c0c624a9c194227e9389cf272
lougeo01 <louis.geoffroypitailler@arm.com>
2025-09-15 21:26:44 +0200

    Add setup script for jellyfin system

---
+++ setup.sh
@@ -0,0 +1,10 @@
+wget https://web9001.nomachine.com/download/9.1/Linux/nomachine_9.1.24_6_amd64.deb
+sudo dpkg -i nomachine_9.1.24_6_amd64.deb
+
+sudo apt install xrdp
+sudo systemctl enable xrdp
+sudo systemctl start xrdp
+cd ~ && mkdir ffsubsync && cd ffsubsync
+python3 -m venv .venv
+source .venv/bin/activate
+pip install ffsubsync
